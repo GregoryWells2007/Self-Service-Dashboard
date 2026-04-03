1c1ba99080b726d5bd22c6663fb8d9eae0899c16
Gregory Wells <gawells@astraltech.xyz>
2026-04-03 18:31:17 -0400

    add more helpter functions

--- src/ldap/ldap_search.go
+++ src/ldap/ldap_search.go
@@ -8,6 +8,18 @@ type LDAPSearch struct {
 	search *ldap.SearchResult
 }
 
+type LDAPEntry struct {
+	entry *ldap.Entry
+}
+
 func (s *LDAPSearch) EntryCount() int {
 	return len(s.search.Entries)
+}
+
+func (s *LDAPSearch) GetEntry(number int) *LDAPEntry {
+	return &LDAPEntry{s.search.Entries[number]}
+}
+
+func (e *LDAPEntry) GetRawAttributeValue(name string) []byte {
+	return e.entry.GetRawAttributeValue(name)
 }
